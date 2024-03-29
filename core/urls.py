06a6a94a5e5bf2991134b38bde04e6bbc88316cc
Bruno Vidal <brunovidal27.19@gmail.com>
2024-03-29 00:30:00 -0300

    Novo Visual

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("", include("pedido_app.urls")),
     path("admin/", admin.site.urls),
     path("integracao/", include("nuvemshop_app.urls")),
 ]
--- pedido_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = "pedido_app"
-
-urlpatterns = [
-    path("", Pedido.as_view(), name="pedido"),
-]
