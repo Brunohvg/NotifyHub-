b5fb52e34fd7de1daf7aa601e45a40d263b8c2b5
Bruno Vidal <brunovidal27.19@gmail.com>
2024-04-02 22:42:44 -0300

    divisão de formulario em blocos

--- user_app/views.py
+++ user_app/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+from django.contrib.auth.signals import user_logged_out
+from django.core.cache import cache
 
 
 # Create your views here.
 def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        print(username, password)
+
     return render(request, "user_app/login.html")
